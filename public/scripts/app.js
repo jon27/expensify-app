ac3eab53849a560cea89373424538af6d557057f
jon27 <jon.cabre@gmail.com>
2018-08-03 11:09:16 -0400

    re adding repo

---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _utils = require('./utils.js');
+
+var _utils2 = _interopRequireDefault(_utils);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log((0, _utils.square)(10)); // // import './utils.js';
+
+console.log((0, _utils.add)(10, 10));
+console.log((0, _utils2.default)(100, 81));
+
---
+++ src/firebase/promises.js
@@ -0,0 +1,18 @@
+const promise = new Promise((resolve, reject)=>{
+	setTimeout(()=> {
+		// resolve('This is my resolved data');
+		reject('Something went wrong!');
+	}, 1500);
+});
+
+console.log('before')
+
+promise.then((data)=>{
+	console.log(data);
+});
+
+promise.then((data)=>{
+	console.log(data);
+});
+
+console.log('after')
