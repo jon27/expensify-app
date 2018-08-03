304da1faa94f4041b8270b37a69ae85049ff1c68
jon27 <jon.cabre@gmail.com>
2018-08-03 11:37:12 -0400

    removed // in url

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,9 +3,9 @@ import * as firebase from 'firebase';
 const config = {
   apiKey: process.env.AIzaSyCHXCJ8YE1sDgL9sWRfafuPlWAxMRe7WLw,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.https://expensify-24166.firebaseio.com/,
+  databaseURL: process.env.https:expensify-24166.firebaseio.com,
   projectId: process.env.expensify-24166,
-  storageBucket: process.env.gs://expensify-24166.appspot.com,
+  storageBucket: process.env.gs:expensify-24166.appspot.com,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
