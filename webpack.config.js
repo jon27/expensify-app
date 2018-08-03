73d7d521e8df164c1a9fea7f9a5dea0cc8b2198a
jon27 <jon.cabre@gmail.com>
2018-08-03 11:46:13 -0400

    modified webapck

--- webpack.config.js
+++ webpack.config.js
@@ -50,9 +50,9 @@ module.exports = (env) => {
       new webpack.DefinePlugin({
         'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.AIzaSyCHXCJ8YE1sDgL9sWRfafuPlWAxMRe7WLw),
         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.https://expensify-24166.firebaseio.com/),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.'https://expensify-24166.firebaseio.com/'),
         'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.expensify-24166),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.gs://expensify-24166.appspot.com),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.gs:'//expensify-24166.appspot.com'),
         'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
       })
     ],
