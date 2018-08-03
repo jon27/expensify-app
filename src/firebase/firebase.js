25bef3f6808736dd834f5a2daa8a8b1d475eaf25
jon27 <jon.cabre@gmail.com>
2018-08-03 13:17:37 -0400

    added proper credentials to firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,15 @@
 import * as firebase from 'firebase';
 
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-};
-
-firebase.initializeApp(config);
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCHXCJ8YE1sDgL9sWRfafuPlWAxMRe7WLw",
+    authDomain: "expensify-24166.firebaseapp.com",
+    databaseURL: "https://expensify-24166.firebaseio.com",
+    projectId: "expensify-24166",
+    storageBucket: "expensify-24166.appspot.com",
+    messagingSenderId: "385388853835"
+  };
+  firebase.initializeApp(config);
 
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
--- webpack.config.js
+++ webpack.config.js
@@ -48,11 +48,11 @@ module.exports = (env) => {
     plugins: [
       CSSExtract,
       new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.AIzaSyCHXCJ8YE1sDgL9sWRfafuPlWAxMRe7WLw),
         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.expensify-24166.firebaseio.com),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.expensify-24166),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.gs:expensify-24166.appspot.com),
         'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
       })
     ],
