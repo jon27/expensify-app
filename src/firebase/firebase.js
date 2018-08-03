05df770adc0d3987ef9f84049700572d68ccbdb0
jon27 <jon.cabre@gmail.com>
2018-08-03 11:44:06 -0400

    modified webapck

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,11 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.AIzaSyCHXCJ8YE1sDgL9sWRfafuPlWAxMRe7WLw,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.expensify-24166.firebaseio.com,
-  projectId: process.env.expensify-24166,
-  storageBucket: process.env.gs:expensify-24166.appspot.com,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
@@ -21,15 +21,15 @@ export { firebase, googleAuthProvider, database as default };
 
 
 
-
-
-
-
 // const config = {
-//   apiKey: process.env.FIREBASE_API_KEY,
+//   apiKey: process.env.AIzaSyCHXCJ8YE1sDgL9sWRfafuPlWAxMRe7WLw,
 //   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.FIREBASE_DATABASE_URL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   databaseURL: process.env.https://expensify-24166.firebaseio.com/,
+//   projectId: process.env.expensify-24166,
+//   storageBucket: process.env.gs://expensify-24166.appspot.com,
 //   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 // };
+
+
+
+
